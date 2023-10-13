6d60dff14003a9ec0f4cea3f6b118d8407656c75
s8983 <s8983@DESKTOP-QBC14O7>
2023-10-13 11:43:02 +0700

    Добавьте файлы проекта.

---
+++ lab-3-task-4/lab-3-task-4.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <clocale>
+
+using namespace std;
+/*
+Лабораторная работа 3, индивидуальное задание 4 - оператор switch. Индивидуальный вариант 24.
+*/
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    int den;
+    cout << "Введите номер дня недели: ";
+    cin >> den;
+    switch (den) {
+    case 1: cout << "Сегодня понедельник ";
+        break;
+    case 2: cout << "Сегодня вторник ";
+        break;
+    case 3: cout << "Сегодня среда ";
+        break;
+    case 4: cout << "Сегодня четверг ";
+        break;
+    case 5: cout << "Сегодня пятница ";
+        break;
+    case 6: cout << "Сегодня суббота ";
+        break;
+    case 7: cout << "Сегодня воскресенье ";
+        break;
+    default:
+        cout << "В неделе только 7 дней" << endl;
+        break;
+    }
+}
